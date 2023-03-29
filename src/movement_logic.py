2ff92066a4ae3b8372f87675b97b2cdc3b0d4ab8
Tom Hilder <tom.hilder.dlhp@gmail.com>
2023-03-29 15:36:44 +1100

    nearly finished adding functions to get all moves like some piece, just pawn takes left

--- src/movement_logic.py
+++ src/movement_logic.py
@@ -123,7 +123,33 @@ class BasicMovements():
         return square_step2
     
     @staticmethod
-    def move_iterator(in_square_label: str, iter_func: Callable) -> Set[str]:
+    def L_move_generator(in_square_label: str, long_move_func: Callable, short_move_func: Callable) -> str:
+        # move 2 squares in long direction
+        square_step1 = long_move_func(long_move_func(in_square_label))
+        # move 1 square in short direction
+        square_step2 = short_move_func(square_step1)
+        # return result
+        return square_step2
+    
+    @staticmethod
+    def L_move_iterator(in_square_label: str, move_func1: Callable, move_func2: Callable) -> Set[str]:
+        # initialise set for possible destinations
+        destination_squares = set()
+        # put the move functions in a list
+        moves = [move_func1, move_func2]
+        # try both combinations
+        for i in range(len(moves)):
+            try:
+                destination_squares.add(
+                    BasicMovements.L_move_generator(in_square_label, moves[i], moves[i-1])
+                )
+            except InvalidMoveError:
+                pass
+        # return results
+        return destination_squares
+
+    @staticmethod
+    def straight_move_iterator(in_square_label: str, iter_func: Callable) -> Set[str]:
         # initialise set for possible destinations
         destination_squares = set()
         # start iteration with starting square
@@ -139,26 +165,108 @@ class BasicMovements():
         return destination_squares
     
     @staticmethod
+    def king_move_iterator(in_square_label: str) -> Set[str]:
+        # initialise set for possible destinations
+        destination_squares = set()
+        # put the possible move functions in a list
+        moves = [
+            BasicMovements.increment_file,
+            BasicMovements.decrement_file,
+            BasicMovements.increment_rank,
+            BasicMovements.decrement_rank,
+            BasicMovements.increment_upwards_diag,
+            BasicMovements.decrement_upwards_diag,
+            BasicMovements.increment_downwards_diag,
+            BasicMovements.decrement_downwards_diag
+        ]
+        # try all moves
+        for i in range(len(moves)):
+            try:
+                destination_squares.add(
+                    moves[i](in_square_label)
+                )
+            except InvalidMoveError:
+                pass
+        # return results
+        return destination_squares        
+    
+    @staticmethod
     def get_all_rook_like(in_square_label: str) -> Set[str]:
         # get all horizontal moves
-        pos_hor = BasicMovements.move_iterator(in_square_label, BasicMovements.increment_file)
-        neg_hor = BasicMovements.move_iterator(in_square_label, BasicMovements.decrement_file)
+        pos_hor = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.increment_file)
+        neg_hor = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.decrement_file)
         # get all vertical moves
-        pos_ver = BasicMovements.move_iterator(in_square_label, BasicMovements.increment_rank)
-        neg_ver = BasicMovements.move_iterator(in_square_label, BasicMovements.decrement_rank)
+        pos_ver = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.increment_rank)
+        neg_ver = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.decrement_rank)
         # return the union of all move sets above
         return pos_hor | neg_hor | pos_ver | neg_ver
     
     @staticmethod
     def get_all_bishop_like(in_square_label: str) -> Set[str]:
-        # get all horizontal moves
-        pos_updiag = BasicMovements.move_iterator(in_square_label, BasicMovements.increment_upwards_diag)
-        neg_updiag = BasicMovements.move_iterator(in_square_label, BasicMovements.decrement_upwards_diag)
-        # get all vertical moves
-        pos_downdiag = BasicMovements.move_iterator(in_square_label, BasicMovements.increment_downwards_diag)
-        neg_downdiag = BasicMovements.move_iterator(in_square_label, BasicMovements.decrement_downwards_diag)
+        # get all moves along the positive diagonal
+        pos_updiag = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.increment_upwards_diag)
+        neg_updiag = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.decrement_upwards_diag)
+        # get all moves along the negative diagonal
+        pos_downdiag = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.increment_downwards_diag)
+        neg_downdiag = BasicMovements.straight_move_iterator(in_square_label, BasicMovements.decrement_downwards_diag)
         # return the union of all move sets above
         return pos_updiag | neg_updiag | pos_downdiag | neg_downdiag
+    
+    @staticmethod
+    def get_all_knight_like(in_square_label: str) -> Set[str]:
+        # get all moves near the positive diagonal
+        pos_updiag = BasicMovements.L_move_iterator(in_square_label, BasicMovements.increment_file, BasicMovements.increment_rank)
+        neg_updiag = BasicMovements.L_move_iterator(in_square_label, BasicMovements.decrement_file, BasicMovements.decrement_rank)
+        # get all moves near the negative diagonal
+        pos_downdiag = BasicMovements.L_move_iterator(in_square_label, BasicMovements.increment_file, BasicMovements.decrement_rank)
+        neg_downdiag = BasicMovements.L_move_iterator(in_square_label, BasicMovements.decrement_file, BasicMovements.increment_rank)
+        # return the union of all move sets above
+        return pos_updiag | neg_updiag | pos_downdiag | neg_downdiag
+    
+    @staticmethod
+    def get_all_king_like(in_square_label: str) -> Set[str]:
+        # get all moves using iterator function
+        moves = BasicMovements.king_move_iterator(in_square_label)
+        # return results
+        return moves
+    
+    @staticmethod
+    def get_all_queen_like(in_square_label: str) -> Set[str]:
+        # get all rook-like moves
+        rook_moves = BasicMovements.get_all_rook_like(in_square_label)
+        # get all bisop-like moves
+        bishop_moves = BasicMovements.get_all_bishop_like(in_square_label)
+        # return the union of rook-like and bishop-like moves
+        return rook_moves | bishop_moves
+    
+    @staticmethod
+    def get_all_pawn_move_like(in_square_label: str, piece_color: str) -> Set[str]:
+        # get the correct movement function, and original rank, based on color
+        if piece_color == "white":
+            move_func = BasicMovements.increment_rank
+            original_rank = "2"
+        elif piece_color == "black":
+            move_func = BasicMovements.decrement_rank
+            original_rank = "7"
+        # get file and rank of piece
+        file, rank = BasicMovements.get_file_and_rank(in_square_label)
+        # initial set set for possible moves
+        moves = set()
+        # add single move
+        try:
+            moves.add(move_func(in_square_label))
+        except InvalidMoveError:
+            pass 
+        # add double move only if on original rank 
+        if rank == original_rank:
+            # this will never throw an exception by construction
+            moves.add(move_func(move_func(in_square_label)))
+        # return moves
+        return moves
+    
+    @staticmethod
+    def get_all_pawn_take_like(in_square_label: str, piece_color: str) -> Set[str]:
+        pass            
         
 class PieceMovements():
     # each method takes starting square and returns two sets
